3ba177774c57afbdf4284259a6d44080c7dd3639
AndreasTpunkt <103647180+AndreasTpunkt@users.noreply.github.com>
2024-09-22 16:47:55 +0200

    Naechster Wurf wird angezeigt

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
+    basic.pause(2000)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Chessboard)
 })
 basic.showIcon(IconNames.Chessboard)
